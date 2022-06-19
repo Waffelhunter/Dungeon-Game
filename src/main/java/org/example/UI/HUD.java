cd9f750ea41025d68093e23361d2f048e6531764
Fabian <Waffelhunter@protonmail.com>
2022-06-19 22:22:40 +0200

    Added HUD

--- src/main/java/org/example/UI/HUD.java
+++ src/main/java/org/example/UI/HUD.java
@@ -2,48 +2,61 @@ package org.example.UI;
 
 import de.gurkenlabs.litiengine.Game;
 import de.gurkenlabs.litiengine.graphics.ImageRenderer;
-import de.gurkenlabs.litiengine.graphics.Spritesheet;
 import de.gurkenlabs.litiengine.gui.GuiComponent;
 import de.gurkenlabs.litiengine.resources.Resources;
 import de.gurkenlabs.litiengine.util.Imaging;
+import org.example.entities.Player;
 
-import javax.tools.Tool;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 
 public class HUD extends GuiComponent {
 
-//    private final BufferedImage HEART = Imaging.scale(Resources.images().get("life.png"),5.0);
-//    private final BufferedImage HEARTEMPTY  = Imaging.scale(Resources.images().get("herzleer.png"),5.0);
-//    //private Image HEARTEMPTY = Toolkit.getDefaultToolkit().getImage("resources/vase/herzleer.png");
-//
-//    private static final int PADDING = 10;
+    private static final BufferedImage HEART = Imaging.scale(Resources.images().get("resources/hud/life.png"), 5.0);
+    private static final BufferedImage HEARTEMPTY = Imaging.scale(Resources.images().get("resources/hud/herzleer.png"), 5.0);
+    private static final BufferedImage ARMOR = Imaging.scale(Resources.images().get("resources/hud/Ruestung.png"), 5.0);
+    private static final BufferedImage ARMOREMPTY = Imaging.scale(Resources.images().get("resources/hud/RuestungLeer.png"), 5.0);
+
+
+    private static final int PADDING = 10;
 
 
     protected HUD() {
-        super(0,0, Game.window().getResolution().getWidth(),Game.window().getResolution().getHeight());
+        super(0, 0, Game.window().getResolution().getWidth(), Game.window().getResolution().getHeight());
     }
 
-//@Override
-//public void render(Graphics2D g) {
-//    super.render(g);
-//    g.setColor(Color.RED);
-//
-//    if (Game.world().environment() == null || Player.instance().isDead()) {
-//        return;
-//
-//    }
-//    this.renderHP(g);
-//}
-//    private void renderHP(Graphics2D g){
-//        double y = Game.window().getResolution().getHeight() - PADDING*2 - HEART.getHeight();
-//        double x = Game.window().getResolution().getWidth() /2.0 - ((Player.instance().getHitPoints().getMax()*(HEART.getWidth()+PADDING)*0.5) - PADDING);
-//        for(int i = 0; i < Player.instance().getHitPoints().getMax();i++){
-//          BufferedImage img = i < Player.instance().getHitPoints().getRelativeCurrentValue() ? HEART : HEARTEMPTY;
-//            ImageRenderer.render(g,img, x + i * img.getWidth() + PADDING, y);
-//
-//        }
-//    }
+    @Override
+    public void render(Graphics2D g) {
+        super.render(g);
+        g.setColor(Color.RED);
+
+        if (Game.world().environment() == null) {
+            return;
+
+        }
+        this.renderHP(g);
+        this.renderArmor(g);
+
+    }
+
+    private void renderHP(Graphics2D g) {
+        double y = Game.window().getResolution().getHeight() - Game.window().getResolution().getHeight() + PADDING * 7 - HEART.getHeight();
+        double x = Game.window().getResolution().getWidth() - ((Player.instance().getHitPoints().getMax() * (HEART.getWidth() + PADDING) * 2.55) - PADDING);
+        for (int i = 0; i < Player.instance().getHitPoints().getMax(); i++) {
+            BufferedImage img = i < Player.instance().getHitPoints().get() ? HEART : HEARTEMPTY;
+            ImageRenderer.render(g, img, x + i * img.getWidth() + PADDING, y);
+
+        }
+    }
+
+    private void renderArmor(Graphics2D g) {
+        double y = Game.window().getResolution().getHeight() - Game.window().getResolution().getHeight() + PADDING * 7 - ARMOR.getHeight();
+        double x = Game.window().getResolution().getWidth() - ((Player.maxArmor * (ARMOR.getWidth() + PADDING) * 0.96) - PADDING);
+        for (int i = 0; i < Player.maxArmor; i++) {
+            BufferedImage img = i < Player.armor ? ARMOR : ARMOREMPTY;
+            ImageRenderer.render(g, img, x + i * img.getWidth() + PADDING, y);
+        }
+    }
 }
 
 
--- src/main/java/org/example/entities/Player.java
+++ src/main/java/org/example/entities/Player.java
@@ -1,10 +1,6 @@
 package org.example.entities;
 
-import de.gurkenlabs.litiengine.entities.CollisionInfo;
-import de.gurkenlabs.litiengine.entities.CombatInfo;
-import de.gurkenlabs.litiengine.entities.Creature;
-import de.gurkenlabs.litiengine.entities.EntityInfo;
-import de.gurkenlabs.litiengine.entities.MovementInfo;
+import de.gurkenlabs.litiengine.entities.*;
 import de.gurkenlabs.litiengine.input.KeyboardEntityController;
 import org.example.abilities.InteractAbility;
 import org.example.abilities.ShootFireballAbility;
@@ -14,31 +10,54 @@ import org.example.abilities.ShootFireballAbility;
 @EntityInfo(width = 32, height = 32)
 @CombatInfo(hitpoints = 10)
 public class Player extends Creature {
-  private static Player instance;
-  private InteractAbility interact;
-  private ShootFireballAbility shoot;
+    public static int armor = 10;
+    public static int maxArmor = 10;
+    private static Player instance;
+    private final InteractAbility interact;
+    private final ShootFireballAbility shoot;
 
-  public static Player instance() {
-    if (instance == null) {
-      instance = new Player();
+    private Player() {
+        super("Hero");
+        addController(new KeyboardEntityController<>(this));
+        interact = new InteractAbility(this);
+        shoot = new ShootFireballAbility(this);
+        addEntityRenderListener(l -> ((Player) l.getEntity()).shoot.render(l.getGraphics()));
+        addEntityRenderListener(l -> ((Player) l.getEntity()).interact.render(l.getGraphics()));
     }
-    return instance;
-  }
 
-  private Player() {
-    super("Hero");
-    addController(new KeyboardEntityController<>(this));
-    interact = new InteractAbility(this);
-    shoot = new ShootFireballAbility(this);
-    addEntityRenderListener(l -> ((Player) l.getEntity()).shoot.render(l.getGraphics()));
-    addEntityRenderListener(l -> ((Player) l.getEntity()).interact.render(l.getGraphics()));
-  }
+    public static Player instance() {
+        if (instance == null) {
+            instance = new Player();
+        }
+        return instance;
+    }
 
-  public void interact() {
-    interact.cast();
-  }
+    public boolean hasArmor() {
+        return armor != 0;
+    }
 
-  public void shoot() {
-    shoot.cast();
-  }
+    public void interact() {
+        interact.cast();
+    }
+
+    public void shoot() {
+        shoot.cast();
+    }
+
+    public void damage(int damage) {
+        if (this.hasArmor()) {
+            int damager = damage;
+            if (armor >= damager) {
+                armor = armor - damager;
+
+            } else {
+                damager = damager - armor;
+                armor = 0;
+                this.hit(damager);
+            }
+        } else {
+            this.hit(damage);
+        }
+    }
 }
+
