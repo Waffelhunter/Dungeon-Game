f7196c214e9452ea1a93915add3f4d9d4d1b7b3f
Fabian <Waffelhunter@protonmail.com>
2022-06-26 21:39:39 +0200

    added CollectablesScreen

---
+++ src/main/java/org/example/Collectables.java
@@ -0,0 +1,73 @@
+package org.example;
+
+import de.gurkenlabs.litiengine.Align;
+import de.gurkenlabs.litiengine.gui.ImageComponent;
+import de.gurkenlabs.litiengine.util.Imaging;
+
+import java.awt.*;
+
+public class Collectables extends CollectablesMenu {
+
+    public static final Color BUTTON_BLACK = new Color(0, 0, 0, 0);
+
+
+    public Collectables(double x, double y, double width, double height, String... items) {
+        super(x, y, width, height, items);
+
+
+    }
+
+    @Override
+    public void prepare() {
+        super.prepare();
+        this.setForwardMouseEvents(true);
+        this.getCellComponents().forEach(comp -> comp.setForwardMouseEvents(true));
+
+        if (!this.getCellComponents().isEmpty()) {
+            this.getCellComponents().get(0).setHovered(true);
+        }
+
+        this.getCellComponents().forEach(comp -> {
+
+                    if (CollectablesManager.imgVisible[comp.getComponentId()]) {
+
+                        comp.setImage(Imaging.scale(CollectablesManager.img[comp.getComponentId()], 4.0));
+                        comp.setImageAlign(Align.RIGHT);
+
+                        comp.getAppearance().setTransparentBackground(true);
+                        comp.getAppearance().setBackgroundColor1(BUTTON_BLACK);
+                        comp.getAppearance().setForeColor(BUTTON_BLACK);
+
+                        comp.onHovered(componentMouseEvent -> {
+                            this.getCellComponents().forEach(com -> {
+                                if (CollectablesManager.imgVisible[com.getComponentId()]) {
+
+                                    com.setImage(Imaging.scale(CollectablesManager.img[com.getComponentId()], 4.0));
+                                    com.getAppearance().setTransparentBackground(true);
+                                    com.setText(null);
+                                }
+                            });
+                            comp.setImage(Imaging.scale(CollectablesManager.img[comp.getComponentId()], 5.0));
+                            comp.setImageAlign(Align.RIGHT);
+                            comp.setText("A Brave Warrior, born to fight and left to die");
+                            comp.setTextAlign(Align.LEFT);
+                            comp.setFontSize(10);
+                            comp.getAppearance().setTransparentBackground(false);
+                            comp.setTextAntialiasing(true);
+
+                        });
+
+
+                    } else {
+
+                        System.out.println("hi");
+                    }
+                 }
+              );
+            }
+
+
+}
+
+
+
---
+++ src/main/java/org/example/CollectablesManager.java
@@ -0,0 +1,32 @@
+package org.example;
+
+import de.gurkenlabs.litiengine.resources.Resources;
+import de.gurkenlabs.litiengine.util.Imaging;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CollectablesManager {
+    public static BufferedImage[] img = new BufferedImage[50];
+    public static boolean[] imgVisible = new boolean[50];
+    private static final BufferedImage HaloChar = Resources.images().get("src/main/resources/Collectables/Halo.png") ;
+
+// nicht hinterfrage erstes Bild das gerendert wird ist nummer 11 ka why
+    public static void init(){
+        for(int j =0; j< imgVisible.length;j++){
+            imgVisible[j] = false;
+        }
+        img[11] = HaloChar;
+        img[12] = HaloChar;
+        img[13] = HaloChar;
+        img[14] = HaloChar;
+        imgVisible[11]= true;
+        imgVisible[12]= true;
+        imgVisible[13]= true;
+        imgVisible[14]= true;
+
+
+    }
+}
