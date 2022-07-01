e3fb9fd87e616bffa029bdbfd4178bd69959a926
Duncan <dunixir@gmail.com>
2022-07-01 22:43:08 +0200

    sus

--- src/main/java/org/example/test.java
+++ src/main/java/org/example/test.java
@@ -1,4 +1,15 @@
 package org.example;
 
 public class test {
+   //. 　　　。　　　　•　 　ﾟ　　。 　　.
+//
+  //          　　　.　　　 　　.　　　　　。　　 。　. 　
+//
+  //          .　　 。　　　　　 ඞ 。 . 　　 • 　　　　•
+//
+  //          　　ﾟ　　 Red was not An Impostor.　 。　.
+//
+  //          　　'　　　 1 Impostor remains 　 　　。
+//
+  //          　　ﾟ　　　.　　　. ,　　　　.　 .
 }
