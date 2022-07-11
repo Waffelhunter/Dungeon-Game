de2eef884e17e8f66174ee05b827dfe5a206de5a
Duncan <dunixir@gmail.com>
2022-07-11 11:17:29 +0200

    Oni Figur in Collectables Screen

--- src/main/java/logic/CollectablesManager.java
+++ src/main/java/logic/CollectablesManager.java
@@ -12,8 +12,10 @@ public class CollectablesManager {
     private static final BufferedImage DoomChar = Resources.images().get("src/main/resources/Collectables/Halo.png") ;
     private static final BufferedImage Kaneki = Resources.images().get("src/main/resources/Collectables/Kaneki.png") ;
     private static final BufferedImage Apex = Resources.images().get("src/main/resources/Collectables/aus_dem_cringen_spiel.png") ;
+    private static final BufferedImage oni = Resources.images().get("src/main/resources/Collectables/oni.png") ;
 
-// nicht hinterfrage erstes Bild das gerendert wird ist nummer 11 ka why
+
+    // nicht hinterfrage erstes Bild das gerendert wird ist nummer 11 ka why
     // Die Figur ist vom Halo
     public static void init(){
         for(int j =0; j< imgVisible.length;j++){
@@ -22,7 +24,7 @@ public class CollectablesManager {
         img[11] = DoomChar;
         img[12] = Kaneki;
         img[13] = Apex;
-        img[14] = DoomChar;
+        img[14] = oni;
         imgVisible[11]= true;
         imgVisible[12]= true;
         imgVisible[13]= true;
